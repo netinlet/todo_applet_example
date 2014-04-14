6f10424870b495bcb3af8b2cf021f79bcf8eafec
Doug Bryant <doug@netinlet.com>
2014-04-14 12:14:23 -0400

    all tests passing

--- test/controllers/my_todo_spec.js
+++ test/controllers/my_todo_spec.js
@@ -3,51 +3,58 @@ var sinon = require('sinon');
 var myTodo = require('../../app/controllers/my_todo');
 
 describe('myTodo', function() {
-
-  beforeEach(function() {
-    angular.mock.module('mytodo', function($provide) {
-      $provide.value('LocalStorageModule', function() {
-      });
-    });
-  }
+  var storage = {};
+  var localStorage = {
+      set: function(name, value) {
+        storage[name] = value;
+      },
+      get: function(name) {
+        return storage[name];
+      }
+    }
 
   it('has a todoList', function() {
     var scope = {};
-    myTodo(scope);
+    myTodo(scope, localStorage);
     expect(scope.todoList).to.be.an('array');
   });
 
-  it('can add an item to the todo list', function() {
-    var scope = {};
-    myTodo(scope);
-    scope.addTodo('coffee');
-    expect(scope.todoList).to.contain('coffee');
+  describe('Adding/syncing todoList', function() {
+    beforeEach(function() {
+      storage = [];
+    });
+
+    it('can add an item to the todo list', function() {
+      var scope = {};
+      myTodo(scope, localStorage);
+      scope.addTodo('coffee');
+      expect(scope.todoList).to.contain('coffee');
+    });
+
+    it('puts the todoList into localStorage when adding a Todo', function(){
+      var scope = {};
+      myTodo(scope, localStorage);
+      scope.addTodo('tea');
+      expect(storage['myTodoList'].indexOf('tea')).not.to.equal(-1);
+    });
+
+    it('adds an item to the todo on submit', function() {
+      var scope = {};
+      myTodo(scope, localStorage);
+      scope.newTodo = 'test';
+      scope.submit();
+      expect(scope.todoList).to.contain('test');
+    });
+
+    it('clears the newTodo when submitting a new todo', function() {
+      var scope = {};
+      myTodo(scope, localStorage);
+      scope.newTodo = 'test';
+      scope.submit();
+      expect(scope.newTodo).to.equal('');
+    });
+
+
   });
-
-  it('puts the todoList into localStorage when adding a Todo', function(){
-    var scope = {};
-    var spy = sinon.spy();
-    myTodo(scope, spy);
-    scope.addTodo('tea');
-    expect(sinon.calledWith('myTodoList', ['tea']).to.be.ok())
-  });
-
-  it('adds an item to the todo on submit', function() {
-    var scope = {};
-    myTodo(scope);
-    scope.newTodo = 'test';
-    scope.submit();
-    expect(scope.todoList).to.contain('test');
-  });
-
-  it('clears the newTodo when submitting a new todo', function() {
-    var scope = {};
-    myTodo(scope);
-    scope.newTodo = 'test';
-    scope.submit();
-    expect(scope.newTodo).to.equal('');
-  });
-
-
 });
 
